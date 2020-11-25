6a312466a4635e7c39399ae1a52e863abf60c5bf
ifes <danielrtrindade1@gmail.com>
2020-11-25 02:15:08 -0300

    main3 findview

--- app/src/main/java/com/example/androidgitexample/MainActivity3.java
+++ app/src/main/java/com/example/androidgitexample/MainActivity3.java
@@ -10,5 +10,7 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        findViewById(R.id.textView2);
     }
 }
